b9a1f8f797d06bcd699fff8db1c576e5d3ae9831
Zoran <kokovic.zoran@gmail.com>
2011-07-21 10:35:35 +0200

    daddy config

--- wp-config.php
+++ wp-config.php
@@ -16,10 +16,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'mdproduction');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'mdproduction');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'password_here');
